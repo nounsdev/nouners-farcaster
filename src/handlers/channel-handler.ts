adb57ded829042a82f76d568f8dfe72fe1493e9d
Milad Nekofar <milad@nekofar.com>
2024-09-28 10:14:46 +0400

    feat(handlers): add `channel-handler` with nouns channel support
    Introduce `channel-handler` to manage nouns channel using Farcaster API.

---
+++ src/handlers/channel-handler.ts
@@ -0,0 +1,51 @@
+import { likeCast } from '@/services/warpcast'
+import { getCastLikes } from '@/services/warpcast/get-cast-likes'
+import { getFeedItems } from '@/services/warpcast/get-feed-items'
+import { recast } from '@/services/warpcast/recast'
+
+/**
+ * Handles the nouns channel in the given environment.
+ * @param env - The environment object.
+ * @returns - A promise that resolves with no value.
+ */
+export async function handleNounsChannel(env: Env) {
+  const { KV: kv } = env
+
+  const nounersLikeThreshold = 2
+  const farcasterUsers: number[] =
+    (await kv.get('nouns-farcaster-users', { type: 'json' })) ?? []
+
+  if (farcasterUsers.length === 0) {
+    return
+  }
+
+  const { items } = await getFeedItems(env, 'nouns', 'unfiltered')
+
+  for (const item of items) {
+    if (item.cast.reactions.count > 0) {
+      let nounersLikeCount = 0
+
+      const { likes } = await getCastLikes(env, item.cast.hash)
+
+      for (const like of likes) {
+        if (farcasterUsers.includes(like.reactor.fid)) {
+          nounersLikeCount += 1
+        }
+      }
+
+      if (nounersLikeCount >= nounersLikeThreshold) {
+        await recast(env, item.cast.hash)
+        await likeCast(env, item.cast.hash)
+      }
+    }
+  }
+}
+
+/**
+ * Handles the channel based on the given environment.
+ * @param env - The environment object containing channel details.
+ * @returns A Promise that resolves when the channel handler has completed execution.
+ */
+export async function channelHandler(env: Env) {
+  await handleNounsChannel(env)
+}
