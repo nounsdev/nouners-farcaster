377dd686692d7644981bdd9127c61492ae23bf9b
Milad Nekofar <milad@nekofar.com>
2024-09-29 18:16:08 +0400

    refactor(cache-handler): split cache functions
    Separate `cacheHandler` into `fetchHolderAddresses`, `fetchDelegateAddresses`, `fetchAndStoreFarcasterUsers`, and `fetchAndStoreFarcasterVoters` for modularity and clarity.

--- src/handlers/cache-handler.ts
+++ src/handlers/cache-handler.ts
@@ -6,14 +6,16 @@ import { getUserByVerification } from '@/services/warpcast/get-user-by-verificat
 import { DateTime } from 'luxon'
 import { map, pipe, sortBy, unique } from 'remeda'
 
+const expirationTtl = 60 * 60 * 24
+
 /**
- * Handles caching of data for Nouns application.
- * @param env - The environment object containing KV storage.
- * @returns - A promise that resolves when caching is complete.
+ * Retrieves the holder's addresses from the provided environment's KV store.
+ * If not found, fetches the accounts and extracts the addresses, then stores them in the KV store.
+ * @param env - The environment object containing the KV store and other configurations.
+ * @returns A promise that resolves to an array of holder's addresses.
  */
-export async function cacheHandler(env: Env) {
+async function fetchHolderAddresses(env: Env) {
   const { KV: kv } = env
-  const expirationTtl = 60 * 60 * 24
 
   let holdersAddresses: string[] =
     (await kv.get('nouns-holders-addresses', { type: 'json' })) ?? []
@@ -23,14 +25,21 @@ export async function cacheHandler(env: Env) {
       accounts,
       map((account) => account.id),
     )
-    await kv.put(
-      'nouns-holders-addresses',
-      JSON.stringify(holdersAddresses),
-      {
-        expirationTtl,
-      },
-    )
+    await kv.put('nouns-holders-addresses', JSON.stringify(holdersAddresses), {
+      expirationTtl,
+    })
   }
+  return holdersAddresses
+}
+
+/**
+ * Retrieves the delegates' addresses from the KV store. If the addresses are not available in the KV store,
+ * it fetches the latest delegate addresses, stores them in the KV store, and then returns them.
+ * @param env - The environment object containing references to external resources like the KV store.
+ * @returns - A promise that resolves to an array of delegate addresses.
+ */
+async function fetchDelegateAddresses(env: Env) {
+  const { KV: kv } = env
 
   let delegatesAddresses: string[] =
     (await kv.get('nouns-delegates-addresses', { type: 'json' })) ?? []
@@ -48,6 +57,21 @@ export async function cacheHandler(env: Env) {
       },
     )
   }
+  return delegatesAddresses
+}
+
+/**
+ * Fetches the Farcaster users associated with holder and delegate addresses
+ * and stores them to the given environment's KV storage.
+ * @param env - The environment object containing the KV storage and necessary configurations.
+ * @returns - A promise that resolves when the fetch and store operation is complete.
+ */
+async function fetchAndStoreFarcasterUsers(env: Env) {
+  const { KV: kv } = env
+
+  const holdersAddresses = await fetchHolderAddresses(env)
+
+  const delegatesAddresses = await fetchDelegateAddresses(env)
 
   let farcasterUsers: number[] =
     (await kv.get('nouns-farcaster-users', { type: 'json' })) ?? []
@@ -79,6 +103,14 @@ export async function cacheHandler(env: Env) {
       expirationTtl,
     })
   }
+}
+
+/**
+ * Fetches Farcaster voters, stores their addresses, and updates the list of Farcaster IDs in a KV store.
+ * @param env - The environment object containing the key-value store.
+ */
+async function fetchAndStoreFarcasterVoters(env: Env) {
+  const { KV: kv } = env
 
   let votersAddresses: string[]
   let farcasterVoters: number[] =
@@ -119,4 +151,14 @@ export async function cacheHandler(env: Env) {
       expirationTtl,
     })
   }
+}
+
+/**
+ * Handles caching of data for Nouns application.
+ * @param env - The environment object containing KV storage.
+ * @returns - A promise that resolves when caching is complete.
+ */
+export async function cacheHandler(env: Env) {
+  await fetchAndStoreFarcasterUsers(env)
+  await fetchAndStoreFarcasterVoters(env)
 }
